d9915779618a3c897472c58cb7e38171b3bfeca7
Daniël <daniel@hi-squeaky-things.nl>
2025-10-28 09:27:15 +0100

    update header

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,10 @@
 #![no_std]
 //!
 //! ```no_run
-//! //       __   ____  ____  __      _  _  ____  __  ____  ____   __  
-//! //      (  ) (_  _)(_  _)(  )    / )( \(  __)(  )(  _ \(    \ /  \
-//! //      / (_/\ )(    )(  / (_/\  \ /\ / ) _)  )(  )   / ) D ((  O )
-//! //      \____/(__)  (__) \____/  (_/\_)(____)(__)(__\_)(____/ \__/
+//! // ⡇ ⡇⢹⠁⢹⠁⡇ ⣏⡉ ⡇⢸⣏⡉⡇⣏⡱⡏⢱⡎⢱
+//! // ⠧⠤⠇⠸ ⠸ ⠧⠤⠧⠤ ⠟⠻⠧⠤⠇⠇⠱⠧⠜⠣⠜
 //! //
-//! //       #no-std optimized wave table synthesizer for embedded devices.
+//! // #no-std optimized wave table synthesizer for embedded devices.
 //! ```
 //!
 //! Example usage:
